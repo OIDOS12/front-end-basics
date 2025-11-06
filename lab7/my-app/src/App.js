76fc918bf1c4f90f5ee62207b8c2d3ea96a9d322
oidos12 <koliesnik.volodymyr@lll.kpi.ua>
2025-11-06 19:36:53 +0200

    components folder

--- lab7/my-app/src/App.js
+++ lab7/my-app/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
+
 function App() {
-	return <div>
-		text
-	</div>;
+	return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
---
+++ lab7/my-app/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Header() {
+    return (
+        <footer>This is the footer</footer>
+    )
+}
+
+export default Header
