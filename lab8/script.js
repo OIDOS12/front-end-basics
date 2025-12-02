d973702a5a849a1a5ae432364aabd27aecf54b8d
oidos12 <koliesnik.volodymyr@lll.kpi.ua>
2025-12-02 20:37:00 +0200

    updated lab 8

--- lab8/script.js
+++ lab8/script.js
@@ -1,10 +1,7 @@
 let slideIndex = 0;
 const slides = document.getElementsByClassName("slide");
-let slideInterval = setInterval(() => changeSlide(1), 5000);
 
 function changeSlide(n) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(() => changeSlide(1), 5000);
     slides[slideIndex].classList.remove("active");
     slideIndex += n;
     if (slideIndex >= slides.length) { slideIndex = 0; }
